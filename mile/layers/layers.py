3744e91f1a5110778edba175874198cf8019d162
Li <glc12125@gmail.com>
2023-08-09 19:35:03 +0800

    update for 3080x2 training

--- mile/layers/layers.py
+++ mile/layers/layers.py
@@ -115,7 +115,7 @@ class ConvBlock(nn.Module):
         if activation == 'relu':
             self.activation = nn.ReLU(inplace=True)
         elif activation == 'lrelu':
-            self.activation = nn.LeakyReLU(0.1)
+            self.activation = nn.LeakyReLU(0.1, inplace=True)
         elif activation == 'elu':
             self.activation = nn.ELU(inplace=True)
         elif activation == 'tanh':
