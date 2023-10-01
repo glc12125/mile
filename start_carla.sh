a17297e7f664dc0b4e1d747998371ea0bb66463f
Liangchuan <li.gu@bdi.tech>
2023-10-01 14:20:18 +0800

    integrated with pygame

--- start_carla.sh
+++ start_carla.sh
@@ -1,1 +1,1 @@
-/media/liangchuan/crucial/carla/CARLA_0.9.11/CarlaUE4.sh -quality-level=Epic -world-port=2000 -resx=800 -resy=600 -preferNvidia
+CUDA_VISIBLE_DEVICES=0 /media/liangchuan/crucial/carla/CARLA_0.9.11/CarlaUE4.sh -quality-level=Epic -world-port=2000 -resx=800 -resy=600 -preferNvidia
