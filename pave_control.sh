800818907b033db28accded3943c58da29d69c5a
Liangchuan <li.gu@bdi.tech>
2023-10-06 15:55:59 +0800

    added showcuts to run in debug mode for carla14

--- pave_control.sh
+++ pave_control.sh
@@ -8,5 +8,8 @@ if [ "$CARLA_VERSION" = "14" ]; then
     python -u pave_automatic_control.0.9.14.py --config-name pave carla_sh_path=/root/media/crucial/carla/CARLA_0.9.14/CarlaUE4.sh agent.mile.ckpt=/home/Development/mile/mile.ckpt port=2000 +mode='single'
 fi
 
+if [ "$CARLA_VERSION" = "14d" ]; then
+    python -u pave_automatic_control.0.9.14.py --config-name pave carla_sh_path=/root/media/crucial/carla/CARLA_0.9.14/CarlaUE4.sh agent.mile.ckpt=/home/Development/mile/mile.ckpt port=2000 +mode='single' show_stats=true log_video=true
+fi
 
 echo "Bash script done."
